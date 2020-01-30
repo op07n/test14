dd60602f094b618d756efe25de92ea4941ac16ca
jperciot <jperciot@oogarden.com>
2020-01-30 09:46:30 +0100

    Adding libraries and links

--- OopWinformsDesigner/OopWinformsDesigner/MainForm.cs
+++ OopWinformsDesigner/OopWinformsDesigner/MainForm.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace OopWinformsDesigner
+﻿namespace OopWinformsDesigner
 {
-    public partial class MainForm : Form
+    public partial class MainForm : DevExpress.XtraEditors.XtraForm
     {
         public MainForm()
         {
